0ae021512a7158530199dae89b50ca61ef8f7521
Richard Hurleu <richard.hurley@mail.helenacollege.edu>
2018-10-03 10:33:40 -0600

    repo

--- HW-6/sketch.js
+++ HW-6/sketch.js
@@ -9,32 +9,33 @@ var col = {
   g: 255,
   b: 255
 }
-
+var x1 = 1 + 2 * 3 % 4;
 
 function setup() {
   createCanvas(windowWidth, 950);
   background('black');
 
+  noCursor();
 
 
 }
 
 function draw() {
+  // stars
+  star.x = random(0, width);
+  star.y = random(0, height);
+  fill(col.r, col.g, col.b);
+  ellipse(star.x, star.y, 1, 1);
 
   star.x = random(0, width);
   star.y = random(0, height);
   fill(col.r, col.g, col.b);
-  ellipse(star.x, star.y, 2, 3);
+  ellipse(star.x, star.y, 1, 1);
 
   star.x = random(0, width);
   star.y = random(0, height);
   fill(col.r, col.g, col.b);
-  ellipse(star.x, star.y, 4, 3);
-
-  star.x = random(0, width);
-  star.y = random(0, height);
-  fill(col.r, col.g, col.b);
-  ellipse(star.x, star.y, 4, 4);
+  //ellipse(star.x, star.y, 1, 1);
 
   push();
   // sun
@@ -59,15 +60,20 @@ function draw() {
   pop();
 
   //planet
+  push();
+  fill('cyan');
+  translate(1600, 550);
+  ellipse(x1, 200, 300, );
+  pop();
 
   push();
   translate(900, 300);
   fill('blue');
   ellipse(-17, -65, 35, 35);
-
+// moon
   translate(-15, -10);
   fill('grey');
-  ellipse(-17, -65, 10, 10); // moon
+  ellipse(-17, -65, 10, 10);
 
 
   pop();
@@ -79,13 +85,13 @@ function draw() {
 
   // Eclipse
   translate(80, 650, );
-  var ax = map(mouseX, mouseY, 50, 75, 390);
+  var ax = map(mouseX, mouseY, -200, 20, 390);
   var ay = 65;
 
 
   fill('black');
   noStroke();
-  ellipse(nfc(ax, 3), ax, ay - 700);
+  ellipse(floor(ax, 1), ax, ay - 700);
 
 
 
