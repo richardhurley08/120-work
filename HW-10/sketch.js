c172af7b28f6d4f7d39f1e40021f58d808fdabbb
Richard Hurleu <richard.hurley@mail.helenacollege.edu>
2018-10-29 21:52:41 -0600

    HW-10

---
+++ HW-10/sketch.js
@@ -0,0 +1,36 @@
+// fizzy
+let circle = [];
+let clicks = 2
+let x = random(width);
+let y = random(hight);
+
+function setup() {
+  noCursor();
+  // frameRate change
+  frameRate(30);
+  createCanvas(windowWidth, windowHeight);
+  fill(clicks, random(255), random(255), random(255));
+  circle.push(0, 0, 40, 20);
+}
+var r = circle[10] * .9;
+// function draw fizzy
+function draw() {
+  ellipse(random(width), random(height), circle[3]);
+  ellipse(random(width), random(height), circle[2]);
+  ellipse(random(width), random(height), circle[3]);
+  ellipse(random(width), random(height), circle[3]);
+}
+// click to change background
+function backgroundChange() {
+  if (clicks == 1) {
+    background(random(255));
+  } else if (clicks == 2) {
+    background(random(255), random(255), random(255), random(255));
+  }
+  inst();
+}
+
+function mousePressed() {
+  backgroundChange();
+  clicks++;
+}
--- HW-8/sketch.js
+++ HW-8/sketch.js
@@ -1,9 +1,9 @@
-// function setup canvas
+    // function setup canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(14);
 }
-// function draw ellipse
+    // function draw ellipse
 function draw() {
   background('black');
   let numPerRow = 55;
@@ -13,7 +13,7 @@ function draw() {
   for (let x = circleSize; x < width; x += pad) {
 
     y += pad;
-    // mouseX color change and twinkle
+      // mouseX color change and twinkle
     for (let x = circleSize; x < width; x += pad) {
       fill(mouseX, random(255), random(255), mouseY, random(255));
       // using ellipse to make star shape
